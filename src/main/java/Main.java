0c743f6fcd388dc0ef493ca087a9f2f10b5300f9
LaserFox1 <lukas.tarantl@gmail.com>
2021-12-02 10:49:50 +0100

    Day 1 of AdventOfCode Part 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(Day1.gaming("src/main/resources/input.txt"));
+        System.out.println(Day1.gaming1("src/main/resources/input1.txt"));
+        System.out.println(Day1.gaming2("src/main/resources/input1.txt"));
     }
 }
